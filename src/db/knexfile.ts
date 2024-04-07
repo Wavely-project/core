37c4c1ebb124601186294b305d3329cfa0dbe10f
Abdallah Elhadad <abdallah31009448@f-eng.tanta.edu.eg>
2024-04-07 20:24:13 +0200

    feat(user): basic structure

---
+++ src/db/knexfile.ts
@@ -0,0 +1,24 @@
+import type { Knex } from 'knex';
+
+// Update with your config settings.
+
+const config: { [key: string]: Knex.Config } = {
+	development: {
+		client: 'mysql2',
+		connection: {
+			host: '127.0.0.1',
+			user: 'root',
+			password: '123456',
+			database: 'wavely',
+		},
+		pool: { min: 2, max: 10 },
+		migrations: {
+			tableName: 'knex_migrations',
+		},
+		seeds: {
+			directory: './seeds',
+		},
+	},
+};
+
+export default config;
