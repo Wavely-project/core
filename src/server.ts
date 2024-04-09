c45904575e425e1254d0695f9d0af5e10c49a228
Abdelrahman Eldabsha <abdodabsha2016@gmail.com>
2024-04-09 02:08:46 +0200

    refactor: deleted comment

--- src/server.ts
+++ src/server.ts
@@ -14,7 +14,6 @@ import { env } from '@/common/utils/envConfig';
 const logger = pino({ name: 'server start' });
 const app: Express = express();
 
-// Set the application to trust the reverse proxy
 app.set('trust proxy', true);
 
 // Middlewares
