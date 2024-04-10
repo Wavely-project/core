9451128d7dad29b725ec547c83f0b2f6c0b4bf9a
Abdallah Elhadad <abdallah31009448@f-eng.tanta.edu.eg>
2024-04-10 23:45:56 +0200

    fix: workflows work?

--- db/db.ts
+++ db/db.ts
@@ -1,5 +1,7 @@
 import knex from 'knex';
 
 import config from '../knexfile';
+const env = process.env.NODE_ENV || 'development';
+console.log('env: ', env);
 
 export default knex(config.development);
