7870c1ecdd269664510b487096b6c65afd886388
Abdallah Elhadad <abdallah31009448@f-eng.tanta.edu.eg>
2024-04-13 22:20:38 +0200

    feat: reference

--- src/socket.ts
+++ src/socket.ts
@@ -1,5 +1,6 @@
 import { Server, Socket } from 'socket.io';
 
+// refer to https://socket.io/docs/v4/server-application-structure#each-file-registers-its-own-event-handlers
 export function listeners(io: Server, socket: Socket) {
 	console.log('Socket connected:', socket.id);
 	io.use((_, next) => {
